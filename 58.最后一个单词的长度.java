925f65049d302c0011ed2756fd89659358a77547
magidragon <magidragon@foxmail.com>
2022-04-13 20:21:47 +0800

    2022-04-13

---
+++ 58.最后一个单词的长度.java
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=java
+ *
+ * [58] 最后一个单词的长度
+ */
+
+// @lc code=start
+class Solution {
+    public int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        while (s.charAt(index) == ' ') {
+            index--;
+        }
+        int wordLength = 0;
+        while (index >= 0 && s.charAt(index) != ' ') {
+            wordLength++;
+            index--;
+        }
+        return wordLength;
+    }
+}
+// @lc code=end
