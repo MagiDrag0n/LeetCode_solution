96805a5b965315479cce29a326b7e8ffca8dde1f
magidragon <magidragon@foxmail.com>
2022-04-18 08:51:46 +0800

    2022-04-18

---
+++ 190.颠倒二进制位.java
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=190 lang=java
+ *
+ * [190] 颠倒二进制位
+ */
+
+// @lc code=start
+public class Solution {
+    public int reverseBits(int n) {
+        int rev = 0;
+        for (int i = 0; i < 32 && n != 0; ++i) {
+            rev |= (n & 1) << (31 - i);
+            n >>>= 1;
+        }
+        return rev;
+    }
+}
+// @lc code=end
