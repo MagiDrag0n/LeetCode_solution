4458f39968c5f7136e82fd11e380325a01117129
magidragon <magidragon@foxmail.com>
2022-06-01 22:08:51 +0800

    2022-06-01

---
+++ 171.excel-表列序号.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=171 lang=java
+ *
+ * [171] Excel 表列序号
+ */
+
+// @lc code=start
+class Solution {
+    public int titleToNumber(String columnTitle) {
+        int number = 0;
+        int multiple = 1;
+        for (int i = columnTitle.length() - 1; i >= 0; i--) {
+            int k = columnTitle.charAt(i) - 'A' + 1;
+            number += k * multiple;
+            multiple *= 26;
+        }
+        return number;
+    }
+}
+// @lc code=end
