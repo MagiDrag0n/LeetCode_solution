3759527fd53252f8c94f1982c8b234d548524aca
magidragon <magidragon@foxmail.com>
2022-06-12 23:47:23 +0800

    2022-06-12

---
+++ 217.存在重复元素.java
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=java
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+// @lc code=end
